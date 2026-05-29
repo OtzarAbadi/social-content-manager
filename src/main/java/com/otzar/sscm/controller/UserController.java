a7b92ee0da3f4c52fb009263ccd2536a78f67c37
Hodaya <Hodaya1rd@gmail.com>
2026-05-29 10:11:01 +0300

    save user token

--- src/main/java/com/otzar/sscm/controller/UserController.java
+++ src/main/java/com/otzar/sscm/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/users")
@@ -37,8 +38,8 @@ public class UserController {
 
             if (user != null) {
 
-                user.setToken("12345");
-
+                user.setToken(String.valueOf(new Random().nextInt()));
+                this.persist.save(user);
                 return new LoginResponse(user);
             }
 
